afac3e27cfe1eaf346c99f619699cddb84fe0614
José Miguel <josleugim@gmail.com>
2016-04-25 18:27:26 -0500

    CORS

--- server/config/express.js
+++ server/config/express.js
@@ -31,4 +31,21 @@ module.exports = function (app, config) {
     }));
 
     app.use(express.static(config.rootPath + '/public'));
+
+    // ## CORS middleware
+// see: http://stackoverflow.com/questions/7067966/how-to-allow-cors-in-express-nodejs
+    var allowCrossDomain = function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE, OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+        // intercept OPTIONS method
+        if ('OPTIONS' == req.method) {
+            res.send(200);
+        }
+        else {
+            next();
+        }
+    };
+    app.use(allowCrossDomain);
 };
