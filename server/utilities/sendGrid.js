5ce15401003143edb15ea97b419acf9ec645d8ce
José Miguel <josleugim@gmail.com>
2016-08-26 12:44:51 -0500

    Return previous code before Callback

--- server/utilities/sendGrid.js
+++ server/utilities/sendGrid.js
@@ -5,7 +5,7 @@
     var config = require('../config/config'),
         sendgrid = require('sendgrid')(config.production.sendgridApiKey);
 
-    exports.sendMail = function (to, from, subject, html , callback) {
+    exports.sendMail = function (to, from, subject, html) {
         var email = new sendgrid.Email();
         console.log("se esta enviando el email.");
         email.addTo(to);
@@ -16,10 +16,10 @@
         sendgrid.send(email, function (err, json) {
             if(err !== null) {
                 console.log('Error sending email: ' + err);
-                callback(false);
+                return false;
             } else{
                 console.log("mensaje enviado")
-                callback(true);
+                return true;
             }
         })
     }
